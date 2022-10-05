6a92756bef70b074d38bd41bceb5fc9a8519af5c
jhhg0 <jhherrera@ccss.sa.cr>
2022-10-05 12:53:45 -0500

    add logic in components and test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,74 @@
+import React, { useState } from "react";
+import PokemonCard from "./components/PokemonCard/PokemonCard";
+import NotFoundCard from "./components/NotFoundCard/NotFoundCard";
+import { getPokemonByIdOrName } from "./services/pokeApi";
+import pokeball from "./assets/pokeball.svg";
 import "./App.css";
 
 function App() {
+  const [pokemon, setPokemon] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [typedPokemon, setTypedPokemon] = useState("");
+
+  function handleChange(event) {
+    setTypedPokemon(event.target.value);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (!typedPokemon) {
+      return;
+    }
+    setIsLoading(true);
+
+    let pokemon = await getPokemonByIdOrName(typedPokemon.toLowerCase());
+
+    if (typeof pokemon === "string") {
+      setError(pokemon);
+      setPokemon(null);
+    } else {
+      setPokemon(pokemon);
+      setError(null);
+    }
+    setIsLoading(false);
+  }
   return (
-  <div className="header content">
-   <div className="header-video">
-    <video loop autoPlay muted>
-      <source src="https://dmwnh9nwzeoaa.cloudfront.net/2022-09/straightforward1.mp4" type="video/mp4" />
-      </video>
-   </div>
-   <div className="header-overlay"></div>
-    <div className="header-content">
-    <h1>Pokemon</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, asperiores.</p>
+    <div className="App">
+      <h1>Pokemon</h1>
+      <p className="Instructions">Type the pokemon name </p>
+      <form className="Form" onSubmit={handleSubmit}>
+        <input
+          className="Input"
+          value={typedPokemon}
+          placeholder="Gotta Catch'Em All!!!!"
+          onChange={handleChange}
+        ></input>
+        <button className="SearchButton" type="submit">
+          {isLoading ? (
+            <>
+              <img
+                id="spinning-pokeball"
+                className="ButtonIcon"
+                src={pokeball}
+                alt="spinning-pokeball"
+              />{" "}
+              loading
+            </>
+          ) : (
+            <>
+             search
+              <img className="ButtonIcon" src={pokeball} alt="pokeball" />{" "}
+            </>
+          )}
+        </button>
+      </form>
+      {pokemon ? (
+        <PokemonCard pokemon={pokemon} />
+      ) : (
+        error && <NotFoundCard error={error} />
+      )}
     </div>
-  </div>
   )
 }
 
